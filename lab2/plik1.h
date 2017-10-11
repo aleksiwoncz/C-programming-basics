34dc0e458efe0f6cedafe51a5d394028fe532e77
aleksiwoncz <aleksandraiwoncz@o2.pl>
2017-10-11 16:40:32 +0200

    zadania

---
+++ lab2/plik1.h
@@ -0,0 +1,14 @@
+//
+//  plik1.h
+//  
+//
+//  Created by Student on 04.10.2017.
+//
+//
+
+#ifndef plik1_h
+#define plik1_h
+
+#include <stdio.h>
+
+#endif /* plik1_h */
---
+++ plik1.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 7;
+    int wynik = a + b;
+    int wynik2 = a * b;
+    int wynik3 = a / b;
+    int wynik4 = a - b;
+    int wynik5 = a % b;
+    printf ("%d", wynik);
+    
+    return 0;
+}
